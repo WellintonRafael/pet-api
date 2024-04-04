326cfda33cef0fcbe53eb9a5842e74813afe163a
WellintonRafael <wellintonrafaelsantos@gmail.com>
2024-04-03 22:10:31 -0300

    Adicionando validação dos atributos com class-validator

--- src/shelter/usecases/dtos/update.shelter.controller.input.ts
+++ src/shelter/usecases/dtos/update.shelter.controller.input.ts
@@ -1,6 +1,17 @@
-export default class UpdateShelterControllerInput{
+import { Contains, IsEmail, IsNotEmpty, IsPhoneNumber, IsString } from "class-validator";
+
+export default class UpdateShelterControllerInput {
+
+    @IsString()
+    @IsNotEmpty()
     shelterName: string;
+
+    @IsPhoneNumber() 
     shelterWhatsApp: string;
+
+    @IsEmail()
     shelterEmail: string;
+
+    @IsPhoneNumber("BR")
     shelterPhone: string;
 }
